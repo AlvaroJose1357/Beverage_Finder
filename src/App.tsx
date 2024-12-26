47516419c3cd45a42ab7898c5d7cbbcc9bde2d89
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2024-12-25 22:19:25 -0500

    mirando el router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">App</h1>
+      <h1>Hello, world!</h1>
+      <p>Welcome to your new single-spa application.</p>
     </>
   );
 }
---
+++ src/pages/IndexPage.tsx
@@ -0,0 +1,10 @@
+export default function IndexPage() {
+  return (
+    <div className="bg-orange-600">
+      <h1 className="text-4xl text-white">
+        Welcome to your new single-spa application!
+      </h1>
+      <p className="text-white">This is the IndexPage component.</p>
+    </div>
+  );
+}
