10237e8af45cf37f0f82f724ce1759607b77b83d
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2024-12-26 23:30:38 -0500

    feat(Header): replace Link with NavLink for improved navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+// import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 export default function Header() {
   return (
     <header className="bg-slate-800">
@@ -7,7 +9,59 @@ export default function Header() {
             {/* logo */}
             <img className="w-32" src="/logo.svg" alt="logotipo" />
           </div>
-          <nav>{/* navegacion entre paginas */}</nav>
+          <nav>
+            {/* navegacion entre paginas */}
+            <ul className="flex space-x-5">
+              {/* usando Link */}
+              {/* <li>
+                <Link
+                  to="/"
+                  className="font-bold uppercase text-white transition-colors hover:text-gray-300"
+                >
+                  Inicio
+                </Link>
+                {/* <a
+                  href="/"
+                  className="text-white hover:text-gray-300 transition-colors"
+                >
+                  Inicio
+                </a> 
+              </li>
+              <li>
+                <Link
+                  to="/favorites"
+                  className="font-bold uppercase text-white transition-colors hover:text-gray-300"
+                >
+                  Favoritos
+                </Link>
+              </li> */}
+              {/* usando NavLink  */}
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "font-bold uppercase text-orange-500 transition-colors hover:text-gray-300"
+                      : "font-bold uppercase text-white transition-colors hover:text-gray-300"
+                  }
+                >
+                  Inicio
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/favorites"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "font-bold uppercase text-orange-500 transition-colors hover:text-gray-300"
+                      : "font-bold uppercase text-white transition-colors hover:text-gray-300"
+                  }
+                >
+                  Favoritos
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
