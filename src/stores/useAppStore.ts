228ff68576e2142411b05e12f157ca70fa562d83
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-01-03 18:55:51 -0500

    feat(favoritesSlice, useAppStore): add favorites slice for managing favorite recipes

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,10 +1,12 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { createRecipeSlice, RecipeSliceType } from "./recipeSlice";
+import { createFavoritesSlice, FavoritesSliceType } from "./favoritesSlice";
 
 // los ...a son los argumentos que tiene create (set,get,api) que se le pasan a los diferentes slice que vamos a crear
-export const useAppStore = create<RecipeSliceType>()(
+export const useAppStore = create<RecipeSliceType & FavoritesSliceType>()(
   devtools((...a) => ({
     ...createRecipeSlice(...a),
+    ...createFavoritesSlice(...a),
   })),
 );
