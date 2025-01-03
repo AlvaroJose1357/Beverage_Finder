29739241ac38de1a05cc05f167121261d8278702
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-01-03 15:48:12 -0500

    feat(DrinkCard, IndexPage): enhance DrinkCard styling and layout on IndexPage

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -6,8 +6,24 @@ type DrinkCardProps = {
 
 export default function DrinkCard({ drink }: DrinkCardProps) {
   return (
-    <div>
-      <h2>{drink.strDrink}</h2>
+    <div className="border shadow-xl">
+      <div className="overflow-hidden">
+        <img
+          src={drink.strDrinkThumb}
+          alt={`Imagen de ${drink.strDrink}`}
+          className="transition-transform hover:rotate-2 hover:scale-110"
+        />
+      </div>
+      <div className="p-5">
+        {/* truncate es por si el texto es muy largo y no se vea bien en la pantalla  */}
+        <h2 className="truncate text-2xl font-black">{drink.strDrink}</h2>
+        <button
+          type="button"
+          className="mt-5 w-full bg-orange-400 p-3 text-lg font-bold text-white hover:bg-orange-600"
+        >
+          Ver Receta
+        </button>
+      </div>
     </div>
   );
 }
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -11,11 +11,11 @@ export default function IndexPage() {
     <>
       <h1 className="text-6xl font-extrabold">Recetas</h1>
       {hasDrink ? (
-        <>
+        <div className="my-10 grid grid-cols-1 gap-5 md:grid-cols-2 2xl:grid-cols-3">
           {drinks.drinks.map((drink) => (
             <DrinkCard key={drink.idDrink} drink={drink} />
           ))}
-        </>
+        </div>
       ) : (
         <>
           <p className="my-10 text-center text-2xl">
