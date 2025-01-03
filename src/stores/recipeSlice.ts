a3789049e1e1fca82be5354c4fd7835ff37d6c47
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-01-03 16:39:57 -0500

    feat(Modal, recipeSlice, Layout): add modal component for displaying selected recipe details and manage modal state

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -10,15 +10,18 @@ export type RecipeSliceType = {
   categories: Categories;
   drinks: Drinks;
   selectedRecipe: Recipe;
+  modal: boolean;
   fetchCategories: () => Promise<void>;
   searchRecipes: (searchFilter: SearchFilter) => Promise<void>;
   selectRecipe: (id: Drink["idDrink"]) => Promise<void>;
+  closeModal: () => void;
 };
 
 export const createRecipeSlice: StateCreator<RecipeSliceType> = (set) => ({
   categories: { drinks: [] },
   drinks: { drinks: [] },
   selectedRecipe: {} as Recipe,
+  modal: false,
   fetchCategories: async () => {
     const categories = await getCategories();
     set(() => ({ categories }));
@@ -31,6 +34,9 @@ export const createRecipeSlice: StateCreator<RecipeSliceType> = (set) => ({
   selectRecipe: async (id) => {
     const selectedRecipe = await getRecipeById(id);
     // console.log(selectedRecipe);
-    set(() => ({ selectedRecipe }));
+    set(() => ({ selectedRecipe, modal: true }));
+  },
+  closeModal: () => {
+    set(() => ({ modal: false, selectedRecipe: {} as Recipe }));
   },
 });
