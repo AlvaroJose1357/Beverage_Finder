89cd524fffe49441328e491c57bff39b23abbc45
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-01-03 21:36:14 -0500

    feat(favoritesSlice, Layout): add loadFromStorage function to initialize favorites from local storage

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -7,6 +7,7 @@ export type FavoritesSliceType = {
   favorites: Recipe[];
   hangleClickFavorite: (recipe: Recipe) => void;
   favoriteExists: (id: Recipe["idDrink"]) => boolean;
+  loadFromStorage: () => void;
 };
 
 // esta es la forma de obtener los datos desde otro slice, pero esta forma debido a su forma en la cual se esta accediendo a los datos no es la mas optima ni la mas recomendada
@@ -38,8 +39,15 @@ export const createFavoritesSlice: StateCreator<
       set((state) => ({ favorites: [...state.favorites, recipe] }));
     }
     createRecipeSlice(set, get, api).closeModal();
+    localStorage.setItem("favorites", JSON.stringify(get().favorites));
   },
   favoriteExists: (id) => {
     return get().favorites.some((favorite) => favorite.idDrink === id);
+  },
+  loadFromStorage: () => {
+    const storeFavorites = localStorage.getItem("favorites");
+    if (storeFavorites) {
+      set({ favorites: JSON.parse(storeFavorites) });
+    }
   },
 });
